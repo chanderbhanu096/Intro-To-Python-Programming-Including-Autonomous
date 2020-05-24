8386adbef04a0278e9f4773989de3407329fb788
Chander Bhanu <chanderbhanu096@gmail.com>
2020-05-24 14:42:31 +0530

    first Programme

---
+++ basicprobabilty.py
@@ -0,0 +1,24 @@
+# importing random library to use to generate random numbers
+import random as rd
+
+num_trials=100
+heads = 0
+tails = 0 
+randominput=0
+head_limit = 0.5
+
+#using for loop to generate 100 random numbers between [0,1] with the help of random function
+for i in range(num_trials):
+    randominput=rd.random()
+
+    # using if state to set the condition for deciding where in  between 
+    # 0 to 1 head lies i.e 0-0.5 or 0.5 to 1
+    if randominput>=0.5:
+        #if random number genreated is between 0.5 and 1 then it will increase head count by 1
+        heads +=1
+    else:
+        #if random number genreated is not between 0.5 and 1 then it will increase tails count by 1
+        tails +=1
+
+print("number of heads ", heads)
+print("number of tails ", tails)
